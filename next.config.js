d7c4b63eb80c7dc05d02e5e211e0126bf740b047
Sanjanabonagiri16 <bonagirisanjana1116@gmail.com>
2024-09-18 11:07:23 +0530

    next.config.js static export

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  // Add any other existing configurations here
+}
+
+module.exports = nextConfig
